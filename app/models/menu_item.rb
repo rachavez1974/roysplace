e75d00f4ff4a113d44c9cffb8c1a8a16fac4c715
Ramon Chavez <rachavez1974@gmail.com>
2019-02-26 20:42:07 -0500

    Attempt to fix nil problem with menu_type

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -13,8 +13,6 @@ class MenuItem < ApplicationRecord
 def self.get_menu_items(*item_menu_type)
   MenuItem.where("current_menu = ? AND (menu_type = ? OR menu_type = ?)", 
   true, item_menu_type.first.to_i, item_menu_type.last.to_i)
-
-  debugger
 end
 
   private
